131caf9d572cd6e1019c27a819a02de18c17a814
Chavez <matthew@el-chavez.me>
2015-01-30 22:38:50 -0800

    Insert values

--- lfu.go
+++ lfu.go
@@ -1,5 +1,7 @@
 package lfu
 
+import "errors"
+
 type LFUCache struct {
 	frequencies map[int]interface{}
 	head        *freqNode
@@ -10,4 +12,18 @@ func NewLFU() *LFUCache {
 		frequencies: make(map[int]interface{}),
 		head:        newFreqNode(),
 	}
+}
+
+func (c *LFUCache) Insert(key int, value interface{}) (bool, error) {
+	_, found := c.frequencies[key]
+	if found {
+		return false, errors.New("Key already exists in cache")
+	}
+	freq := c.head.next
+	if freq.value != 1 {
+		freq = getNewNode(1, c.head, freq)
+	}
+	freq.items.Add(key)
+	c.frequencies[key] = value
+	return true, nil
 }
--- lfu_test.go
+++ lfu_test.go
@@ -10,3 +10,20 @@ func TestNewLFU(t *testing.T) {
 		t.Fatalf("Expected no frequencies on new cache got %+v", len(cache.frequencies))
 	}
 }
+
+func TestInsert(t *testing.T) {
+	cache := NewLFU()
+	success, e := cache.Insert(2, []byte("user-login:34:123456789"))
+	if !success || e != nil {
+		t.Fatalf("Expected to be inserted into cache but got %+v - %+v", success, e)
+	}
+}
+
+func TestInsert_duplicate(t *testing.T) {
+	cache := NewLFU()
+	cache.Insert(2, []byte("user-signout:34:987654321"))
+	success, e := cache.Insert(2, []byte("user-login:34:123456789"))
+	if success || e == nil {
+		t.Fatalf("Expected error trying to insert duplicate key")
+	}
+}
